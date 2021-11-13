a2adc8cc00b2658f3332fffb08e3727632e585b9
Alexandre <alexandre.martins@osmose-solutions.com>
2021-11-13 15:29:58 +0100

    init pages

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# if you are deploying to https://MartinsAlexandre.github.io/genetic
+git push -f git@github.com:MartinsAlexandre/genetic.git master:gh-pages
 
 cd -
