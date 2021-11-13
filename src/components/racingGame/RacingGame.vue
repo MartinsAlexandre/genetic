fc836ad1077a72663b4c98d34c8893e791851312
Alexandre <alexandre.martins@osmose-solutions.com>
2021-11-13 15:38:26 +0100

    init racingGame

---
+++ src/components/racingGame/RacingGame.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        <h1>racing game</h1>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "racingGame",
+    // https://dev.to/lukegarrigan/genetic-algorithms-in-javascript-mc3
+}
+</script>
