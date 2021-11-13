2f9272d97922786a38bf7e4a2b38dd51d7d01912
Alexandre <alexandre.martins@osmose-solutions.com>
2021-11-13 15:56:12 +0100

    init

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,24 @@
 set -e
 
 # build
-npm run build
+# npm run build
+
+vue-cli-service build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://MartinsAlexandre.github.io/genetic
-git push -f git@github.com:MartinsAlexandre/genetic.git master:gh-pages
+# git push -f git@github.com:MartinsAlexandre/genetic.git master:gh-pages
 
-cd -
+# cd -
